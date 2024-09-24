1af979cfbe9cb842460f1cd7d0f2ed731ce7969a
Patrick Kipkoech <patrickwayy@gmail.com>
2024-09-24 13:39:49 +0300

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork()
+
+function wrapAdjective(wrapper = "*"){
+    return function(adjective = "special"){
+        return `You are ${wrapper}${adjective}${wrapper}!`
+    }
+}
+let wrapstar= wrapAdjective('*')
+console.log(wrapstar("a hard worker"))
+
+let wraps= wrapAdjective('||')
+console.log(wraps("a dedicated programmer"))
